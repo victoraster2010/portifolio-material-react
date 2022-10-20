81bdefd17e4ef4a8c58991388563c61bec90319c
Victor Hugo Souza <victoraster2014@gmail.com>
2022-10-20 17:36:46 -0300

    feat:Adicionado o react-router-dom e setup de rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import {BrowserRouter} from 'react-router-dom';
+import { AppRoutes } from './routes';
 export const App = () => {
   return (
-    <div className="App">
-    Test
-    </div>
+    <BrowserRouter>
+        <AppRoutes/>
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+export const AppRoutes = () => {
+
+  return (
+    <Routes>
+          <Route path="/" element={<p>Página padrão</p>}/>
+          <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+};
