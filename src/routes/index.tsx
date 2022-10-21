09b17897dac581b486bfb8980631fcec1805d924
Victor Hugo Souza <victoraster2014@gmail.com>
2022-10-20 23:02:48 -0300

    feat:Adiciona o material UI(MUI)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,9 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
-
 export const AppRoutes = () => {
 
   return (
     <Routes>
-          <Route path="/" element={<p>Página padrão</p>}/>
+          <Route path="/" element={<p>lmao</p>}/>
           <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
